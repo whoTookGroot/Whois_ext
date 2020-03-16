c990e8c8a6c7a3ecfb68b729c7f4da3be69dc572
Erik-Grootendorst <1mdagroot@gmail.com>
2020-03-16 10:56:46 -0700

    Update sweeper with cronjob instructions

--- server/sql/sweeper.sh
+++ server/sql/sweeper.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-
+# Cron job to run, on command line enter:
+# crontab -e
+# In the editor, enter the following cron directive. there are 6 fields, blank fields are indicated 
+# by an asteriks (*). Fields are separated by a single space.
+# The following directive runs the sweeper.sh script every day at 1am::
+# 0 1 * * * /path/to/sweeper.sh
 echo "Removing entries for db:table -> domains:domains..."
 echo 'Delete from domains;alter sequence domains_id_seq restart;' | /usr/local/bin/psql domains
 echo "Entries removed"
